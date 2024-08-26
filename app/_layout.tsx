0731699f041786b6d29cfdea788df45698999ddf
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-26 12:57:09 +0300

    project skeleton

---
+++ app/_layout.tsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <View style={styles.container}>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
